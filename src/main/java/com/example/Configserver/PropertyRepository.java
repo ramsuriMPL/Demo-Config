a264a8fb9163065b3d62a43a2912bbb89bce96ed
Ram Suri <ramsuri@mplgaming.com>
2022-03-02 13:13:11 +0530

    property files added and controller modified

--- src/main/java/com/example/Configserver/PropertyRepository.java
+++ src/main/java/com/example/Configserver/PropertyRepository.java
@@ -2,5 +2,5 @@ package com.example.Configserver;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepositry  extends CrudRepository<User,Integer> {
+public interface PropertyRepository extends CrudRepository<properties,Integer> {
 }
--- src/main/java/com/example/Configserver/properties.java
+++ src/main/java/com/example/Configserver/properties.java
@@ -3,7 +3,50 @@ package com.example.Configserver;
 import javax.persistence.Entity;
 
 @Entity
-public class ConfigEntity {
+public class properties {
     private String application;
+    private String profile;
+    private String label;
+    private String key;
+    private String value;
 
+    public String getApplication() {
+        return application;
+    }
+
+    public void setApplication(String application) {
+        this.application = application;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
 }
