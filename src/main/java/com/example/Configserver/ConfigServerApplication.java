544bc34749cbe689bbf1d3dc02fe42988243982f
Ram Suri <ramsuri@mplgaming.com>
2022-03-03 11:21:35 +0530

    recent changes

--- src/main/java/com/example/Configserver/ConfigServerApplication.java
+++ src/main/java/com/example/Configserver/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.example.Configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableConfigServer
+@Configuration
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
